239ca2ba06439b94393bea0be96d8e4ff9d10fe6
João Vitor dos Santos Ritter <joao.ritter@ufn.edu.br>
2026-02-26 14:57:59 -0300

    ajuste na centralizacao da task page

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -8,12 +8,34 @@ function App() {
   );
 
 
-
   //useEffect é um hook que executa efeitos colaterais
   //neste caso, sempre que atualizar o state de task = atualizar o local storage
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks)) //converte task (lista de objetos) em json
   }, [tasks])
+
+
+
+  //PARA CHAMAR API E PEGAR TEREFAS
+  /*
+  useEffect(() => {
+    const fetchTasks = async () => {
+      //chamar api
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10', {
+        method: 'GET'
+      });
+      //pegar dados que ela retorna
+      const data = await response.json();
+      console.log(data);
+
+      //armazenar/persistir os dados
+      setTasks(data)
+    };
+    fetchTasks();
+
+  }, []); //array vazio = executa apenas uma vez, quando o componente for montado
+
+  */
 
 
   function onTaskClick(taskId) {
---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+
+function Input({ type, placeholder, value, onChange }) {
+    return (
+        <input
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+        />
+    )
+}
+
+export default Input;
